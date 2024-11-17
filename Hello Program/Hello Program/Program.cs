1bc817db8e3635699fafb9f56e7d7935ae0751a3
Naimul Islam <129820411+naimulislamsaikat@users.noreply.github.com>
2024-11-17 22:00:14 +0600

    First Program of C#

---
+++ Hello Program/Hello Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! It's my 1st Program"); // for print any message 
+            Console.Write("Press 'Enter' for terminate the terminal...");
+            Console.ReadLine(); // for holding the terminal
+        }
+    }
+}
